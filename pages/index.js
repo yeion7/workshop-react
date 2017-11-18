8d7214f5074ba531c45aa3371f07a861e0de8241
Yeison Daza <jahir045@gmail.com>
2017-11-18 10:09:26 -0500

    '#update: about speaker'

--- pages/index.js
+++ pages/index.js
@@ -14,16 +14,24 @@ export default () => (
     <img src="/static/react.png" alt="Logo React" width="350px" />
     <br />
     <code>git clone git@github.com:yeion7/workshop-react.git</code>
+
+    <div>
+      <h2>About speaker</h2>
+      <h3>🤓 Yeison Daza</h3>
+      <h3>🐦 @yeion7</h3>
+      <h3>😸😼🐱 Cats </h3>
+      <h3>🤡 Clown</h3>
+    </div>
     <style jsx>{`
       main {
         background: black;
         min-width: 100vw;
         min-height: 100vh;
         text-align: center;
+        font-family: 'Open Sans', Arial, Helvetica, sans-serif;
       }
 
       h1 {
-        font-family: 'Open Sans', Arial, Helvetica, sans-serif;
         font-size: 10em;
         margin: 0;
         padding: 0;
@@ -33,6 +41,11 @@ export default () => (
       code {
         background: white;
         padding: 10px;
+      }
+
+      h2,
+      h3 {
+        color: white;
       }
     `}</style>
   </main>
